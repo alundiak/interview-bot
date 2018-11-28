d7cf42b75f742a7e74d1eda044d6d1cb15bf5aab
Andrii Lundiak <landike@gmail.com>
2018-11-28 01:37:51 +0100

    Initial commit of TypeScript-written, Echo-based, Quiz-like code for InterviewBot

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# interview-bot
-Interview Bot
+# IV-bot
+Interview Bot, which maintains a simple quiz-like dialog for initial Candidate Interview.
+
+
+## Name
+- IV?
+- IV-bot?
+- InterviewBot?
+- InterviewMaster?
+- Interviewer?
+- InterviewHelper?
+- InterviewQuiz?
+
+
+## Idea
+
+Bot will provide a quiz with questions. Interview Person should provide `type` of quiz: `JavaScript`, `React`, `Java`, `TBD`. Then start answering questions. Bot will analyze answers and give feedback right away. Logic for making decision is very easy - only one question is correct, and Bot is programmed to know it, and if user reply is the same as. expected by Bot - then question is passed (OK).
+
+
+## Tech Stack
+- This bot has been created using [Microsoft Bot Framework](https://dev.botframework.com), it's a simple echo bot with state.
+- Bot logic hosted in NodeJS-like server, using [`restify`](https://www.npmjs.com/package/restify).
+- Main code written in [TypeScript](https://www.typescriptlang.org) >= `v3.1.x`.
+
+
+## How to use Bot?
+
+- Add in Skype
+- Add in Facebook Messenger
+- Add in Telegram
+- todo
+
+
+## How to develop Bot?
+
+- See `DEVELOPMENT_NOTES.md`
+- Read more about MS Bot Framework.
+- Bot uses Microsoft Application (id and password)
+
+
+## Deployment
+- Easy: Bot logic can be hosted in either local network as available host, or dedicated computer or any public free host. Not much of efforts needed, it's just quiz-like behavior.
+- Complex: Bot can be hosted on Heroku, but some delays can be in responses, then we need to use Cafeinne app, which will cause Heroku later on to suspend Heroku account.
+- Complicated: Bot can be hosted od MS dedicated Azure service. But it can be paid with time.
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import * as restify from 'restify';
+import * as path from 'path';
+import { config } from 'dotenv';
+import { BotFrameworkAdapter, MemoryStorage, ConversationState } from 'botbuilder';
+import { BotConfiguration, IEndpointService } from 'botframework-config';
+// import { MyBot } from './bot';
+import { SuggestedActionsBot } from './bot-suggested-actions';
+const ENV_FILE = path.join(__dirname, '..', '.env');
+const env = config({ path: ENV_FILE });
+const DEV_ENVIRONMENT = 'development';
+const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
+
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open IV-bot.bot file in the Emulator.`);
+});
+
+const BOT_FILE = path.join(__dirname, '..', (process.env.botFilePath || ''));
+
+let botConfig;
+try {
+    botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
+} catch (err) {
+    console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
+    console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
+    console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.\n\n`);
+    process.exit();
+}
+
+const endpointConfig = <IEndpointService>botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
+const a = {
+    appId: endpointConfig.appId || process.env.microsoftAppID,
+    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
+};
+console.log(a);
+const adapter = new BotFrameworkAdapter(a);
+
+const memoryStorage = new MemoryStorage();
+// CAUTION: You must ensure your product environment has the NODE_ENV set
+//          to use the Azure Blob storage or Azure Cosmos DB providers.
+// const { BlobStorage } = require('botbuilder-azure');
+// Storage configuration name or ID from .bot file
+// const STORAGE_CONFIGURATION_ID = '<STORAGE-NAME-OR-ID-FROM-BOT-FILE>';
+// // Default container name
+// const DEFAULT_BOT_CONTAINER = '<DEFAULT-CONTAINER>';
+// // Get service configuration
+// const blobStorageConfig = botConfig.findServiceByNameOrId(STORAGE_CONFIGURATION_ID);
+// const blobStorage = new BlobStorage({
+//     containerName: (blobStorageConfig.container || DEFAULT_BOT_CONTAINER),
+//     storageAccountOrConnectionString: blobStorageConfig.connectionString,
+// });
+// conversationState = new ConversationState(blobStorage);
+
+const conversationState = new ConversationState(memoryStorage);
+
+// const myBot = new MyBot(conversationState);
+const myBot = new SuggestedActionsBot();
+
+adapter.onTurnError = async (context, error) => {
+    console.error(`\n [onTurnError]: ${error}`);
+    // Send a message to the user
+    context.sendActivity(`Oops. Something went wrong!`);
+    // Clear out state
+    await conversationState.load(context);
+    await conversationState.clear(context);
+    // Save state changes.
+    await conversationState.saveChanges(context);
+};
+
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        await myBot.onTurn(context);
+    });
+});
+
